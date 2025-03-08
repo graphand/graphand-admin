5fa302e81c8f64c2b3423e08b19b5970e0ae541f
pierrecabriere <hello@pierrecabriere.fr>
2025-03-08 00:31:33 -0500

    minor improvemens

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
+import { createServerClient } from "@/lib/graphand-server";
 
-const PUBLIC_ROUTES = ["/auth/login", "/auth/register"];
+const isPublicRoute = (pathname: string) => {
+  return (
+    pathname.startsWith("/auth") ||
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api")
+  );
+};
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const authToken = request.cookies.get("auth_token");
   const { pathname } = request.nextUrl;
 
-  // Check if user is trying to access a protected route without being logged in
-  if (
-    !authToken &&
-    !PUBLIC_ROUTES.includes(pathname) &&
-    !pathname.startsWith("/_next")
-  ) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
+  const client = await createServerClient();
+  const me = await client.me();
 
-  // Check if user is trying to access login/register while already logged in
-  if (authToken && PUBLIC_ROUTES.includes(pathname)) {
-    return NextResponse.redirect(new URL("/", request.url));
+  // Check if user is trying to access a protected route without being logged in
+  if (isPublicRoute(pathname)) {
+    if (me) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (!me) {
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+    }
   }
 
   return NextResponse.next();
